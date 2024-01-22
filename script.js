362f1d79c426fb2a6a9dd1166d80471c432b85ce
Artem <kvar65@yandex.ru>
2024-01-22 19:11:30 +0300

    added hw-7

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ function season() {
         console.log('Такого времени года не бывает :(')
     }
 }
+
+function words() {
+    let wordsArray = [
+			'Яблоко',
+			'Груша',
+			'Дыня',
+			'Виноград',
+			'Персик',
+			'Апельсин',
+			'Мандарин',
+		];
+    wordsArray = wordsArray.sort(() => Math.random() - 0.5);
+    alert(wordsArray);
+    let first = prompt('Чему равнялся первый элемент массива?');
+    let second = prompt('Чему равнялся последний элемент массива?');
+    if (first === wordsArray[0] && second === wordsArray[wordsArray.length - 1]) {
+        alert('Все верно! Вы молодец!');
+    } else if (first === wordsArray[0] || second === wordsArray[wordsArray.length - 1]) {
+		alert('Вы были близки к победе!');
+	} else {
+		alert('Неверно');
+	}
+}
